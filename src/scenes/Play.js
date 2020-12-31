a4c1dcc3ec907684f516fce941d5deb4c8c9f8f7
Ivanov <civanov.me@gmail.com>
2020-12-31 18:07:00 +0200

    fix: clean up

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -10,16 +10,6 @@ export default class Play extends Scene {
     footer.x = - window.innerWidth / 2;
     footer.y = window.innerHeight / 2 - footer.height;
     this.addChild(footer);
-
-    console.log(footer);
-
-    // create a sprite with the gamepad asset as texture and add it to the stage
-    const sprite = Sprite.from('gamepad');
-
-    this.addChild(sprite);
-    sprite.anchor.set(0.5);
-
-    gsap.to(sprite.scale, { x: 1.1, y: 1.1, duration: 1, repeat: -1, yoyo: true, ease: "power2.out" });
   }
 
   /**
